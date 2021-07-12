676c4fb5fbdc1f38d01dfdc0291f05c1b046df42
hjKim <loveu306@hanmail.net>
2021-07-12 16:12:18 +0900

    todo page commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
 
 const createGlobalStyle = createGlobalStyle`
     body {
@@ -12,7 +15,11 @@ function App() {
     return (
         <>
             <GlobalStyle />
-            <TodoTemplate>hello</TodoTemplate>
+            <TodoTemplate>
+                <TodoHead />
+                <TodoList />
+                <TodoCreate />
+            </TodoTemplate>
         </>
     );
 }
